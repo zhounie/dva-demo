061bd9f0ec6b59f8435a4b946283fc5f351fccb0
zn <iszhounie@aliyun.com>
2020-07-03 17:46:51 +0800

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+
+
+import Login from ''
+
+const routes = app => [
+    {
+        path: '/sign',
+        title: '登录',
+        indexRoute: '/sign/login',
+        childRoutes: [
+
+        ]
+    }
+]
+
+// export default app => 

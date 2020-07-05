5ad508671a99b2fca3adf2eea3491f03df7087ee
zhounie <iszhounie@aliyun.com>
2020-07-05 23:33:01 +0800

    修改名称

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import Layout from './routes/IndexPage';
 import Login from './routes/Login/index'
 import UserManage from './routes/UserManage/index'
 
@@ -9,9 +9,9 @@ function RouterConfig({ history }) {
     <Router history={history}>
       <Switch>
         <Route path="/login" exact component={Login} />
-        <IndexPage>
+        <Layout>
           <Route path="/userManage" component={UserManage} />
-        </IndexPage>
+        </Layout>
       </Switch>
     </Router>
   );
